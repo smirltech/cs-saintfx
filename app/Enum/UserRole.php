93689504ec0ded19e0eb5bc0271987e39e2c248d
Marien Mupenda <marien.mupenda@gmail.com>
2022-10-15 17:13:37 +0200

    update caissier

--- app/Enum/UserRole.php
+++ app/Enum/UserRole.php
@@ -8,8 +8,9 @@ enum UserRole: string
     case super_admin = 'super_admin';
     case admin = 'admin';
     case operateur = 'operateur';
-    case appariteur = 'appariteur';
-    case sga = 'secretaire_general';
+    case caissier = 'caissier';
+    case prefet = 'prefet';
+    case promoteur = 'promoteur';
 
     public function permissions(): array
     {
@@ -27,24 +28,23 @@ enum UserRole: string
             self::admin => [
                 RolePermission::create_user->name,
             ],
-            self::appariteur => [
-                RolePermission::create_etudiant->name,
-                RolePermission::edit_etudiant->name,
-                RolePermission::delete_etudiant->name,
+            self::prefet => [
                 RolePermission::view_etudiant->name,
 
 
                 RolePermission::create_faculte->name,
                 RolePermission::create_promotion->name,
             ],
-            self::sga => [
+            self::promoteur => [
                 RolePermission::view_etudiant->name,
                 RolePermission::create_user->name,
             ],
             self::operateur => [
-                RolePermission::create_etudiant->name,
                 RolePermission::view_etudiant->name,
-            ]
+            ],
+            self::caissier => [
+                RolePermission::view_etudiant->name,
+            ],
 
         };
 
