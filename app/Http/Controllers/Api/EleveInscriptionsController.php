b67f2a1fcfdc55069fc87824162b6ec4400cf38d
Marien Mupenda <marien.mupenda@gmail.com>
2022-10-22 22:03:11 +0200

    update elve api

--- app/Http/Controllers/Api/EleveInscriptionsController.php
+++ app/Http/Controllers/Api/EleveInscriptionsController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Eleve;
+use Orion\Concerns\DisableAuthorization;
+use Orion\Http\Controllers\Controller;
 
-class EleveInscriptionsController
+class EleveInscriptionsController extends Controller
 {
+    use DisableAuthorization;
 
     protected $model = Eleve::class;
     protected $relation = 'inscriptions';
--- app/Http/Controllers/Api/EleveResponsablesController.php
+++ app/Http/Controllers/Api/EleveResponsablesController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Eleve;
+use Orion\Concerns\DisableAuthorization;
+use Orion\Http\Controllers\Controller;
 
-class EleveResponsablesController
+class EleveResponsablesController extends Controller
 {
+    use DisableAuthorization;
 
     protected $model = Eleve::class;
     protected $relation = 'responsables';
