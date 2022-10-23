d4a7150e368aad21f4bd8cfe0bddd666213aa4b4
Marien Mupenda <marien.mupenda@gmail.com>
2022-10-23 13:31:16 +0200

    remove useles api routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,4 @@ use Orion\Facades\Orion;
 
 Route::group(['as' => 'api.'], function () {
     Orion::resource('eleves', EleveController::class)->only(['index', 'show']);
-    /* Orion::hasManyResource('eleves', 'inscriptions', EleveInscriptionsController::class)->only(['index', 'show']);
-     Orion::hasManyResource('eleves', 'reponsables', EleveResponsablesController::class)->only(['index', 'show']);*/
 });
