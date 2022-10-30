f6405e7636ef39dd619fcd86e3f45402b8fed147
Marien Mupenda <marien.mupenda@gmail.com>
2022-10-30 20:38:28 +0100

    add filiere resource

--- app/Http/Resources/FiliereResource.php
+++ app/Http/Resources/FiliereResource.php
@@ -8,6 +8,14 @@ class FiliereResource extends JsonResource
 {
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'nom' => $this->nom,
+            'code' => $this->code,
+            'option_id' => $this->option_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'classes' => ClasseResource::collection($this->whenLoaded('classes')),
+        ];
     }
 }
