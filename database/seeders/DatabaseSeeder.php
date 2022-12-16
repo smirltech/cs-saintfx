fbf9bd6060254db05e5d4944572ae6bc52f4fd29
Marien Mupenda <marien.mupenda@gmail.com>
2022-12-16 11:27:11 +0200

    move catories to factory seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,19 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\ClasseEnseignant;
-use App\Models\Cours;
-use App\Models\CoursEnseignant;
-use App\Models\Devoir;
-use App\Models\DevoirEleve;
-use App\Models\Eleve;
-use App\Models\Enseignant;
-use App\Models\Inscription;
-use App\Models\Presence;
-use App\Models\Responsable;
-use App\Models\ResponsableEleve;
-use App\Models\Resultat;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 use Schema;
 
@@ -40,31 +27,11 @@ class DatabaseSeeder extends Seeder
             ClasseSeeder::class,
         ]);
 
-
         // if local env
-        if (app()->environment('local')) {
-            User::factory(10)->create();
-
-            Eleve::factory(30)->create();
-            Inscription::factory(30)->create();
-
-            Responsable::factory(30)->create();
-            ResponsableEleve::factory(30)->create();
-
-            Enseignant::factory(30)->create();
-            ClasseEnseignant::factory(10)->create();
-
-            Cours::factory(30)->create();
-            CoursEnseignant::factory(20)->create();
-
-            Presence::factory(30)->create();
-
-            Resultat::factory(30)->create();
-
-            Devoir::factory(30)->create();
-            DevoirEleve::factory(30)->create();
-
-
+        if (!app()->isProduction()) {
+            $this->call([
+                FactorySeeder::class,
+            ]);
         }
 
         Schema::enableForeignKeyConstraints();
---
+++ database/seeders/FactorySeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ClasseEnseignant;
+use App\Models\Cours;
+use App\Models\CoursEnseignant;
+use App\Models\Devoir;
+use App\Models\DevoirEleve;
+use App\Models\Eleve;
+use App\Models\Enseignant;
+use App\Models\Inscription;
+use App\Models\Presence;
+use App\Models\Responsable;
+use App\Models\ResponsableEleve;
+use App\Models\Resultat;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class FactorySeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+
+        User::factory(5)->create();
+
+        Eleve::factory(10)->create();
+        Inscription::factory(10)->create();
+
+        Responsable::factory(10)->create();
+        ResponsableEleve::factory(10)->create();
+
+        Enseignant::factory(10)->create();
+        ClasseEnseignant::factory(10)->create();
+
+        Cours::factory(10)->create();
+        CoursEnseignant::factory(20)->create();
+
+        Presence::factory(10)->create();
+
+        Resultat::factory(10)->create();
+
+        Devoir::factory(10)->create();
+        DevoirEleve::factory(10)->create();
+
+    }
+}
