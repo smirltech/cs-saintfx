b5fa8d0254e595d4dc890ac70dc42a38b53ea167
Marien Mupenda <marien.mupenda@gmail.com>
2022-12-21 15:10:40 +0200

    add remmember false

--- config/adminlte.php
+++ config/adminlte.php
@@ -191,7 +191,7 @@ return [
     'sidebar_mini' => 'lg',
     'sidebar_collapse' => true,
     'sidebar_collapse_auto_size' => false,
-    'sidebar_collapse_remember' => true,
+    'sidebar_collapse_remember' => false,
     'sidebar_collapse_remember_no_transition' => false,
     'sidebar_scrollbar_theme' => 'os-theme-light',
     'sidebar_scrollbar_auto_hide' => 'l',
