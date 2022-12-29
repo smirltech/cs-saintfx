8ad292c600f440035ce6177b6972de03b04c7132
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-12-29 18:50:16 +0200

    minor bug fix

--- app/Traits/TopMenuPreview.php
+++ app/Traits/TopMenuPreview.php
@@ -2,6 +2,10 @@
 
 namespace App\Traits;
 use App\Models\Annee;
+use App\Models\Classe;
+use App\Models\ClasseEnseignant;
+use App\Models\CoursEnseignant;
+use App\Models\Inscription;
 use Illuminate\Support\Facades\Event;
 use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
@@ -21,6 +25,40 @@ trait TopMenuPreview
                     'key' => 'annee_encours',
                     'text' => "Année scolaire",
                     'label' => $aa->nom,
+                    'url' => '#',
+                    'label_color' => 'secondary',
+                    'topnav' => true,
+                ]);
+            }
+
+            if (!$event->menu->itemKeyExists('eleves_inscrits')) {
+                $event->menu->add([
+                    'key' => 'eleves_inscrits',
+                    'text' => "Élèves",
+                    'label' => Inscription::getCurrentInscriptions()->count(),
+                    'url' => '#',
+                    'label_color' => 'secondary',
+                    'topnav' => true,
+                ]);
+            }
+
+            if (!$event->menu->itemKeyExists('annee_classes')) {
+                $event->menu->add([
+                    'key' => 'annee_classes',
+                    'text' => "Classes",
+                    'label' => Classe::all()->count(),
+                    'url' => '#',
+                    'label_color' => 'secondary',
+                    'topnav' => true,
+                ]);
+            }
+
+            if (!$event->menu->itemKeyExists('enseignants')) {
+
+                $event->menu->add([
+                    'key' => 'enseignants',
+                    'text' => "Enseignants",
+                    'label' => ClasseEnseignant::all()->count() + CoursEnseignant::all()->count(),
                     'url' => '#',
                     'label_color' => 'secondary',
                     'topnav' => true,
