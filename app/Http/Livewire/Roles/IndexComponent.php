f9990c727a5f469bb1420b8bacac11bf374c3fa5
Marien Mupenda <marien.mupenda@gmail.com>
2023-01-23 10:38:14 +0200

    add roles

---
+++ app/Http/Livewire/Roles/IndexComponent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire\Roles;
+
+use App\Models\Role;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Component;
+
+class IndexComponent extends Component
+{
+
+    public Collection $roles;
+
+    public function mount(): void
+    {
+        $this->roles = Role::all();
+    }
+
+    public function render(): Factory|View|Application
+    {
+        return view('livewire.roles.index-component')->with([
+            'title' => 'Roles',
+        ]);
+    }
+}
---
+++ resources/views/livewire/roles/index-component.blade.php
@@ -0,0 +1,78 @@
+@section('content_header')
+    <div class="row">
+        <div class="col-6">
+            <h1 class="ms-3">Liste de consommables</h1>
+        </div>
+
+        <div class="col-6">
+            <ol class="breadcrumb float-right">
+                <li class="breadcrumb-item"><a href="{{ route('scolarite') }}">Accueil</a></li>
+                <li class="breadcrumb-item active">Consommables</li>
+            </ol>
+        </div>
+    </div>
+
+@stop
+<div class="p-0 container-fluid ">
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-7">
+                <div class="card-header">
+                    <div class="m-0 box_header">
+                        <div class="main-title">
+                            <h3 hidden class="m-0">{{__($title)}}</h3>
+                        </div>
+                        <div class="erning_btn d-flex float-right">
+                            <a href="{{route('users.create')}}" class="btn btn-outline-primary btn-sm"><i
+                                    class="fa fa-plus"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3 card-body">
+                    <!-- table-responsive -->
+                    <div class="table-responsive-md">
+                        <table class="table">
+                            <thead class="table-header">
+                            <tr>
+                                <th>#</th>
+                                <th>Nom</th>
+                                <th>Description</th>
+                                <th>Permissions</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($roles as $role)
+                                <tr>
+                                    <td>
+                                        {{ $loop->iteration }}
+                                    </td>
+                                    <td>
+                                        {{$role->display_name}}
+                                    </td>
+                                    <td>
+                                        {{$role->description}}
+                                    </td>
+                                    <td>
+                                        {{$role->display_permissions}}
+                                    </td>
+                                    <td>
+
+                                        <a href="{{route('users.edit', $role->id)}}"
+                                           class="btn btn-outline-primary btn-sm">
+                                            <i class="fa fa-edit"></i>
+                                        </a>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
