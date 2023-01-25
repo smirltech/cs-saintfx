8063a8e6f07880895b45f96de95b0ba9e6db1d0a
Marien Mupenda <marien.mupenda@gmail.com>
2023-01-25 07:07:54 +0200

    remove custom error pages

--- resources/views/errors/401.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('errors.minimal')
-
-@section('title', __('Unauthorized'))
-@section('code', '401')
-@section('message', __('Unauthorized'))
-@section('url', __('Please contact the system administrator.'))
