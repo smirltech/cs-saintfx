d1290f56302fdea43a4800eb5a9160de5970f995
Marien Mupenda <marien.mupenda@gmail.com>
2023-02-18 10:20:45 +0200

    remove toolbar to pdf

--- resources/views/livewire/bibliotheque/ouvrages/read.blade.php
+++ resources/views/livewire/bibliotheque/ouvrages/read.blade.php
@@ -1,1 +1,2 @@
-<object data-readonly="true" data="{{$ouvrage->getFirstMediaUrl()}}" width="100%" height="500"></object>
+{{--<object data-readonly="true" data="{{$ouvrage->getFirstMediaUrl()}}#toolbar=0" width="100%" height="100%"></object>--}}
+<object data-readonly="true" data="{{$ouvrage->getFirstMediaUrl()}}#toolbar=0" width="100%" height="100%"></object>
