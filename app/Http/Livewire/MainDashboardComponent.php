62c1137b004f40d0e25ac2c3db9ebcdeda583805
francisnnumbi <francis@smirl.org>
2023-02-19 18:16:15 +0200

    updating main dashboard

--- app/Http/Livewire/MainDashboardComponent.php
+++ app/Http/Livewire/MainDashboardComponent.php
@@ -15,9 +15,43 @@ class MainDashboardComponent extends Component
 {
     use TopMenuPreview;
 
+    public $boxes = [];
+
     public function mount()
     {
        // return redirect(LoginController::redirectTo());
+
+        $this->boxes = [
+            [
+                'title' => "Scolarité",
+                'text' => 'Gestion école',
+                'icon' => 'fas fa-fw fa-user-graduate',
+                'url' => route('scolarite'),
+                'theme' => 'danger',
+            ],
+            [
+                'title' => "Finance",
+                'text' => 'Gestion finance',
+                'icon' => 'fas fa-fw fa-arrow-trend-up',
+                'url' => route('finance'),
+                'theme' => 'primary',
+            ],
+            [
+                'title' => "Logistique",
+                'text' => 'Gestion logistique',
+                'icon' => 'fas fa-fw fa-recycle',
+                'url' => route('logistique.materiels'),
+                'theme' => 'warning',
+            ],
+            [
+                'title' => "Bibliotheque",
+                'text' => 'Gestion bibliotheque',
+                'icon' => 'fas fa-fw fa-book',
+                'url' => route('bibliotheque'),
+                'theme' => 'success',
+            ],
+
+        ];
     }
 
     public function render(): Factory|View|Application
