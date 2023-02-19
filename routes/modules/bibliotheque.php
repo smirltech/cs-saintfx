f9651a03fde00fc28e7fd4fcf879c93a36c2da49
Marien Mupenda <marien.mupenda@gmail.com>
2023-02-19 15:13:22 +0200

    fix biblio imports

--- routes/modules/bibliotheque.php
+++ routes/modules/bibliotheque.php
@@ -1,37 +1,32 @@
 <?php
 
-use App\Http\Livewire\Bibliotheque\Auteur\AuteurIndexComponent;
-use App\Http\Livewire\Bibliotheque\Auteur\AuteurShowComponent;
-use App\Http\Livewire\Bibliotheque\Etiquette\EtiquetteIndexComponent;
-use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageCreateComponent;
-use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageIndexComponent;
-use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageReadComponent;
-use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageShowComponent;
-use App\Http\Livewire\Bibliotheque\OuvrageCategory\OuvrageCategoryIndexComponent;
-use App\Http\Livewire\Bibliotheque\OuvrageCategory\OuvrageCategoryShowComponent;
 use App\Http\Livewire\Bibliotheque;
+use App\Http\Livewire\Bibliotheque\Auteur;
+use App\Http\Livewire\Bibliotheque\Ouvrage;
+use App\Http\Livewire\Bibliotheque\OuvrageCategory;
+use App\Http\Livewire\Bibliotheque\Tags;
 
 Route::get('bibliotheque', Bibliotheque\DashboardComponent::class)->name('bibliotheque')->middleware('auth');
 
 Route::prefix('bibliotheque')->middleware(['auth:web'])->as('bibliotheque.')->group(function () {
 
     // Étiquettes
-    Route::get('etiquettes', EtiquetteIndexComponent::class)->name('etiquettes');
+    Route::get('etiquettes', Tags\TagsIndexComponent::class)->name('etiquettes');
 
     // Auteurs
-    Route::get('auteurs', AuteurIndexComponent::class)->name('auteurs');
-    Route::get('auteurs/{auteur}', AuteurShowComponent::class)->name('auteurs.show');
+    Route::get('auteurs', Auteur\AuteurIndexComponent::class)->name('auteurs');
+    Route::get('auteurs/{auteur}', Auteur\AuteurShowComponent::class)->name('auteurs.show');
 
     // Categories
-    Route::get('categories', OuvrageCategoryIndexComponent::class)->name('categories');
-    Route::get('categories/{category}', OuvrageCategoryShowComponent::class)->name('categories.show');
+    Route::get('categories', Bibliotheque\OuvrageCategory\OuvrageCategoryIndexComponent::class)->name('categories');
+    Route::get('categories/{category}', OuvrageCategory\OuvrageCategoryShowComponent::class)->name('categories.show');
 
     // Ouvrages
-    Route::get('ouvrages', OuvrageIndexComponent::class)->name('ouvrages.index');
-    Route::get('ouvrages/create', OuvrageCreateComponent::class)->name('ouvrages.create');
-    Route::get('ouvrages/{ouvrage}', OuvrageShowComponent::class)->name('ouvrages.show');
-    Route::get('ouvrages/{ouvrage}/edit', OuvrageCreateComponent::class)->name('ouvrages.edit');
-    Route::get('ouvrages/{ouvrage}/read', OuvrageReadComponent::class)->name('ouvrages.read');
+    Route::get('ouvrages', Ouvrage\OuvrageIndexComponent::class)->name('ouvrages.index');
+    Route::get('ouvrages/create', Ouvrage\OuvrageCreateComponent::class)->name('ouvrages.create');
+    Route::get('ouvrages/{ouvrage}', Ouvrage\OuvrageShowComponent::class)->name('ouvrages.show');
+    Route::get('ouvrages/{ouvrage}/edit', Ouvrage\OuvrageCreateComponent::class)->name('ouvrages.edit');
+    Route::get('ouvrages/{ouvrage}/read', Ouvrage\OuvrageReadComponent::class)->name('ouvrages.read');
 
 });
 
