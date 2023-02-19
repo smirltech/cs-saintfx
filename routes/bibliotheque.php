ca75d046aeb3e519b978b5a1a0f0d36fe3062155
Francis Nduba Numbi <francisnnumbi@yandex.com>
2023-02-19 14:16:11 +0200

    separation de la page d'accueil principale de celles des modules

---
+++ routes/bibliotheque.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Livewire\Bibliotheque\Auteur\AuteurIndexComponent;
+use App\Http\Livewire\Bibliotheque\Auteur\AuteurShowComponent;
+use App\Http\Livewire\Bibliotheque\Etiquette\EtiquetteIndexComponent;
+use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageCreateComponent;
+use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageIndexComponent;
+use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageReadComponent;
+use App\Http\Livewire\Bibliotheque\Ouvrage\OuvrageShowComponent;
+use App\Http\Livewire\Bibliotheque\OuvrageCategory\OuvrageCategoryIndexComponent;
+use App\Http\Livewire\Bibliotheque\OuvrageCategory\OuvrageCategoryShowComponent;
+
+Route::prefix('bibliotheque')->middleware(['auth:web'])->as('bibliotheque.')->group(function () {
+
+    // Étiquettes
+    Route::get('etiquettes', EtiquetteIndexComponent::class)->name('etiquettes');
+
+    // Auteurs
+    Route::get('auteurs', AuteurIndexComponent::class)->name('auteurs');
+    Route::get('auteurs/{auteur}', AuteurShowComponent::class)->name('auteurs.show');
+
+    // Categories
+    Route::get('categories', OuvrageCategoryIndexComponent::class)->name('categories');
+    Route::get('categories/{category}', OuvrageCategoryShowComponent::class)->name('categories.show');
+
+    // Ouvrages
+    Route::get('ouvrages', OuvrageIndexComponent::class)->name('ouvrages.index');
+    Route::get('ouvrages/create', OuvrageCreateComponent::class)->name('ouvrages.create');
+    Route::get('ouvrages/{ouvrage}', OuvrageShowComponent::class)->name('ouvrages.show');
+    Route::get('ouvrages/{ouvrage}/edit', OuvrageCreateComponent::class)->name('ouvrages.edit');
+    Route::get('ouvrages/{ouvrage}/read', OuvrageReadComponent::class)->name('ouvrages.read');
+
+});
+
---
+++ routes/finance.php
@@ -0,0 +1,32 @@
+<?php
+use App\Http\Livewire\Finance;
+
+Route::prefix('finance')->middleware(['auth:web'])->as('finance.')->group(function () {
+
+    // Rapport
+    Route::get('rapports', Finance\Rapport\RapportIndexComponent::class)->name('rapports');
+
+    //Revenu
+    Route::get('revenus', Finance\Revenu\RevenuIndexComponent::class)->name('revenus');
+
+    //Depense
+    Route::get('depenses', Finance\Depense\DepenseIndexComponent::class)->name('depenses');
+
+    Route::get('depense-types', Finance\DepenseType\DepenseTypeIndexComponent::class)->name('depense-types');
+    Route::get('depense-types/{depenseType}', Finance\DepenseType\DepenseTypeShowComponent::class)->name('depense-types.show');
+
+    //Frais
+    Route::get('frais', Finance\Frais\FraisIndexComponent::class)->name('frais');
+
+    //Perception
+    Route::get('perceptions/create', Finance\Perception\PerceptionCreateComponent::class)->name('perceptions.create');
+    Route::get('perceptions/classe_create', Finance\Perception\PerceptionClasseCreateComponent::class)->name('perceptions.classe-create');
+    Route::get('perceptions/{perception}/edit', Finance\Perception\PerceptionEditComponent::class)->name('perceptions.edit');
+    Route::get('perceptions', Finance\Perception\PerceptionIndexComponent::class)->name('perceptions');
+    Route::get('caisse', Finance\Perception\CaisseComponent::class)->name('caisse');
+
+    //Perception
+    //Route::get('eleves', Finance\Eleve\EleveIndexComponent::class)->name('eleves');
+    // Route::get('eleves/{id}', Finance\Eleve\EleveShowComponent::class)->name('eleves.show');
+});
+
