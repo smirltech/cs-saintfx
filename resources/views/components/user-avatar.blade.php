f47e0b1f4a1149dca148fee54fd0f26ad3b70288
Marien Mupenda <marien.mupenda@gmail.com>
2023-03-10 11:28:31 +0200

    add edit

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -3,8 +3,26 @@
     @if($edit)
         wire:click="$emit('showModal', 'profile.edit-avatar-modal', '{{ class_basename($model) }}','{{ $model->id }}')"
     @endif
-    class="text-center">
+    class="img text-center">
+    @if($edit)
+        <span class="badge badge-primary p-2">
+        <i class="fa fa-camera-alt"></i>
+   </span>
+    @endif
     <img class="profile-user-img img-fluid img-circle"
          src="{{$model->avatar}}"
          alt="User profile picture">
 </div>
+<style>
+    .img {
+        position: relative;
+    }
+
+    .img span {
+        top: 50%;
+        left: 50%;
+        position: absolute;
+        transform: translate(-50%, -50%);
+        color: white;
+    }
+</style>
