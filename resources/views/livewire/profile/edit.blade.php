6ca26d5dd5342da5055c839dbfa01cc8dee3afa4
Marien Mupenda <marien.mupenda@gmail.com>
2023-03-10 19:28:21 +0200

    update edit

--- resources/views/livewire/profile/edit.blade.php
+++ resources/views/livewire/profile/edit.blade.php
@@ -26,8 +26,8 @@
                     <div class="card-body box-profile">
                         <div class="d-flex align-items-center flex-column">
                             <div class="sw-13 position-relative mb-3">
-                                {{$user->media}}
-                                {{$user->avatar}}
+                                @dd($user->media, $user->getFirstMediaUrl(),$user->avatar)
+                                {{-- <x-avatar :model="$user"/>--}}
                                 {{-- <x-avatar :model="$user"/>--}}
                             </div>
                             <div class="h5 mb-0">{{$user->name}}</div>
