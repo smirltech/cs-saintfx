76125993c0eda6b9825caeefd87dfa596b5d95b6
Marien Mupenda <marien.mupenda@gmail.com>
2023-04-12 15:40:45 +0200

    demande moddisplayÃ

--- resources/views/livewire/notification/nofication-index-component.blade.php
+++ resources/views/livewire/notification/nofication-index-component.blade.php
@@ -6,15 +6,16 @@
                     @foreach($notifications as $notification)
                         <div class="col-md-6">
                             <div
-                                class="alert {{$notification->unread()?'alert-warning':'alert-info'}} alert-dismissible"
-                                wire:click="openNotification('{{$notification->id}}')">
+                                class="alert {{$notification->unread()?'alert-warning':'alert-info'}} alert-dismissible">
                                 <button wire:click="deleteNotification('{{$notification->id}}')" type="button"
                                         class="close" data-dismiss="alert" aria-hidden="true">×
                                 </button>
-                                <h4><i class="fa {{$notification->read()?'fa-envelope-open':'fa-envelope'}}"></i>
+                                <h4 wire:click="openNotification('{{$notification->id}}')">
+                                    <i class=" fa {{$notification->read()?'fa-envelope-open':'fa-envelope'}}"></i>
                                     {{$notification->data['title']??''}}
                                 </h4>
-                                <p>{{$notification->data['message']??''}}
+                                <p wire:click="openNotification('{{$notification->id}}')">
+                                    {{$notification->data['message']??''}}
                                     <br>
                                     <small>{{$notification->created_at->diffForHumans()}}</small></p>
                             </div>
