9e1a8e55cc1c8ab7f148accc96cca95c9915406c
Marien Mupenda <marien.mupenda@gmail.com>
2023-05-19 09:49:42 +0200

    uncomment delete responsable

--- resources/views/livewire/scolarite/responsables/modals/crud.blade.php
+++ resources/views/livewire/scolarite/responsables/modals/crud.blade.php
@@ -6,7 +6,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">Ajouter Responsable</h4>
-                <button type="button" class="close" data-dismiss="modal"
+                <button wire:click="$emit('onModalClosed')" type="button" class="close" data-dismiss="modal"
                         aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
