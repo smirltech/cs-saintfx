3bcbd370c56a92415922467666585aeb9b5344eb
Marien Mupenda <marien.mupenda@gmail.com>
2024-11-02 16:36:27 +0200

    add filter perceptions

--- app/Http/Livewire/Finance/Perception/PerceptionIndexComponent.php
+++ app/Http/Livewire/Finance/Perception/PerceptionIndexComponent.php
@@ -5,44 +5,66 @@ namespace App\Http\Livewire\Finance\Perception;
 use App\Enums\UserRole;
 use App\Http\Livewire\BaseComponent;
 use App\Models\Annee;
+use App\Models\Classe;
 use App\Models\Consommable;
 use App\Models\Depense;
 use App\Models\Enseignant;
+use App\Models\Frais;
 use App\Models\Inscription;
 use App\Models\Materiel;
 use App\Models\Perception;
 use App\Models\User;
 use App\Traits\TopMenuPreview;
 use App\View\Components\AdminLayout;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Redirector;
 
 class PerceptionIndexComponent extends BaseComponent
 {
     use TopMenuPreview;
     use LivewireAlert;
 
-    public $perceptions = [];
     public $perception;
+
+    public string $classe_id = '';
+    public string $annee_id = '';
+    public string $frais_id = '';
 
     public function mount(): void
     {
         $this->authorize('viewAny', Perception::class);
 
+        $this->classe_id = request()->get('classe_id') ?: '';
+        $this->frais_id = request()->get('frais_id') ?: '';
+
         $this->annee_id = Annee::id();
+        $this->classes = Classe::all();
+        $this->frais = Frais::all();
 
     }
 
     public function getBoxesProperty(): array
     {
-        $perceptionsUSD =Perception::whereDevise('USD')->sum('montant');
-        $perceptionsCDF = Perception::whereDevise('CDF')->sum('montant');
 
-        $perceptionsTodayUSD =Perception::ofToday()->whereDevise('USD')->sum('montant');
-        $perceptionsTodayCDF = Perception::ofToday()->whereDevise('CDF')->sum('montant');
+        $perceptionQuery = Perception::when($this->classe_id, function ($q) {
+            $q->whereHas('inscription', function ($q) {
+                $q->where('classe_id', $this->classe_id);
+            });
+        })->when($this->frais_id, function ($q) {
+            $q->where('frais_id', $this->frais_id);
+        });
 
-        $perceptionsMeTodayUSD =Perception::ofToday()->whereDevise('USD')->whereUserId(Auth::id())->sum('montant');
-        $perceptionsMeTodayCDF = Perception::ofToday()->whereDevise('CDF')->whereUserId(Auth::id())->sum('montant');
+        $perceptionsUSD = $perceptionQuery->whereDevise('USD')->sum('montant');
+        $perceptionsCDF = $perceptionQuery->whereDevise('CDF')->sum('montant');
+
+        $perceptionsTodayUSD = $perceptionQuery->whereDevise('USD')->sum('montant');
+        $perceptionsTodayCDF = $perceptionQuery->whereDevise('CDF')->sum('montant');
+
+        $perceptionsMeTodayUSD = $perceptionQuery->whereDevise('USD')->whereUserId(Auth::id())->sum('montant');
+        $perceptionsMeTodayCDF = $perceptionQuery->whereDevise('CDF')->whereUserId(Auth::id())->sum('montant');
 
 
         return [
@@ -83,11 +105,22 @@ class PerceptionIndexComponent extends BaseComponent
 
     public function render()
     {
-        $perceptionsRequest = Perception::where('annee_id', $this->annee_id);
-        $this->perceptions = $perceptionsRequest->latest()->get();
 
         return view('livewire.finance.perceptions.index', ['perceptions' => $this->perceptions])
             ->layout(AdminLayout::class, ['title' => 'Liste de Perceptions']);
+    }
+
+    public function getPerceptionsProperty(): Collection
+    {
+        $perceptionsRequest = Perception::when($this->classe_id, function ($q) {
+            $q->whereHas('inscription', function ($q) {
+                $q->where('classe_id', $this->classe_id);
+            });
+        })->when($this->frais_id, function ($q) {
+            $q->where('frais_id', $this->frais_id);
+        });
+
+        return $this->perceptions = $perceptionsRequest->latest()->get();
     }
 
     public function getSelectedPerception(Perception $perception): void
@@ -106,6 +139,14 @@ class PerceptionIndexComponent extends BaseComponent
 
         $this->onModalClosed();
 
+    }
+
+    public function search(): RedirectResponse|Redirector
+    {
+        return redirect()->route('finance.perceptions', [
+            'classe_id' => $this->classe_id,
+            'frais_id' => $this->frais_id,
+        ]);
     }
 
 
--- resources/views/livewire/finance/perceptions/index.blade.php
+++ resources/views/livewire/finance/perceptions/index.blade.php
@@ -56,6 +56,65 @@
 
 @stop
 <div>
+    <div class="card">
+        <div class="card-header">
+            <form wire:submit.prevent="search" class="mt-1">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <x-form::select
+                                    placeholder="Classe"
+                                    wire:model="classe_id"
+                                    :options="$classes"/>
+                            </div>
+                            <div class="col-md-3">
+                                <x-form::select
+                                    placeholder="Frais"
+                                    wire:model="frais_id"
+                                    :options="$frais"/>
+                            </div>
+
+
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <x-form::input
+                                            type="date"
+                                            wire:model="date_from"/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <x-form::input
+                                            type="date"
+                                            wire:model="date_to"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-6">
+                                @can("perceptions.create")
+                                    <x-form::button wire:click="search" href="#">
+                                        <span class="fas fa-search"></span>
+                                    </x-form::button>
+                            </div>
+                            {{-- <div class="col-md-6">
+                                 <button disabled
+                                         class="btn btn-outline-success m-1">
+                                     <span class="fas fa-file-pri"></span>
+                                 </button>
+                             </div>--}}
+
+                            @endcan
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
     <div class="row g-2">
         @foreach ($this->boxes as $box)
             <div class="col-md-3" bis_skin_checked="1">
