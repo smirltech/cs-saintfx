c6d4fac7b446b3e086399bed2b0aec8edf90f5cf
Marien Mupenda <marien.mupenda@gmail.com>
2025-10-15 06:13:18 +0200

    fix modal on etiquette

--- resources/views/livewire/bibliotheque/tags/create-tag-modal.blade.php
+++ resources/views/livewire/bibliotheque/tags/create-tag-modal.blade.php
@@ -1,4 +1,4 @@
-<x-modals::modal-form>
+<x-modals::form title="Create Tag">
     <div class="row">
         <div class="form-group col-md-12 col-sm-12">
             <x-form::input
@@ -13,5 +13,5 @@
             <button type="submit" class="btn btn-outline-primary mr-3">Soumettre</button>
         </div>
     </x-slot:footer>
-</x-modals::modal-form>
+</x-modals::form>
 
